f8bd52bc6e8572dc3f18293866529d3829b7f90e
thkernel <salut.amos@gmail.com>
2023-09-06 04:57:07 +0000

    update

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  #default from: "salut.amos@gmail.com"
-  default from: "SuiteCourrier<#{SmtpServerSetting.take.user_name}>" if SmtpServerSetting.take.present?
+ default from: 'from@example.com'
   layout 'mailer'
 end
